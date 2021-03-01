df2823592f12af77313592933af646163b0bca20
Anwar Ahmed <anwar.ahmed+retail@gmail.com>
2021-03-01 00:15:00 -0500

    upgrade to .net 5

--- DoubleA.DataAccess/DoubleA.DataAccess/DataContexts/DataContext.cs
+++ DoubleA.DataAccess/DoubleA.DataAccess/DataContexts/DataContext.cs
@@ -25,7 +25,10 @@ namespace DoubleA.DataAccess.DataContexts
         public bool HasChanges => ChangeTracker.HasChanges();
 
         /// <inheritdoc/>
-        public new IQueryable<TEntity> Query<TEntity>() where TEntity : class => Set<TEntity>().AsNoTracking();
+        public IQueryable<TEntity> Tracked<TEntity>() where TEntity : class => Set<TEntity>();
+
+        /// <inheritdoc/>
+        public IQueryable<TEntity> Untracked<TEntity>() where TEntity : class => Set<TEntity>().AsNoTracking();
 
         /// <inheritdoc/>
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
